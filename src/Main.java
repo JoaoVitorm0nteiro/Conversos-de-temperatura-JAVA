e7282f3f5fef54b7c5c60d6d820d2b53dd9ddbe5
JoaoVitorm0nteiro <jv147pao@gmail.com>
2022-02-12 21:33:29 -0300

    resto da estrutura de repetição

--- src/Main.java
+++ src/Main.java
@@ -7,14 +7,29 @@ public class Main {
 
         System.out.println("1 - Cº -> F");
         System.out.println("2 - Cº -> K");
-        System.out.println("Digite qual tipo de conversão você quer efetuar: ");
-        double ctrlvar = teclado.nextDouble();
-        if (ctrlvar == 1) {
-            System.out.println("Digite a temperatura: ");
-            double C = teclado.nextDouble();
-            double conversao = C * 1.8 + 32;
-            System.out.println("O resultado é: " + conversao);
+        //menu simples
+        String pergunta;
 
-        }
+        do {
+            System.out.print("Digite qual tipo de conversão você quer efetuar: ");
+            double ctrlvar = teclado.nextDouble();
+            //usuario escolhe quais das operações disponiveis ele quer realizar
+            if (ctrlvar == 1) {
+                System.out.print("Digite a temperatura: ");
+                double C = teclado.nextDouble();
+                double conversao = C * 1.8 + 32;
+                System.out.println("O resultado é: " + conversao + " F");
+            } else if (ctrlvar == 2) {
+                System.out.print("Digite a temperatura: ");
+                double C = teclado.nextDouble();
+                double conversao = C + 273.15;
+                System.out.println("O resultado é: " + conversao + " K");
+                //caso ele escolha uma opção indisponivel entra no else
+            } else
+                System.out.println("Opção invalida.");
+            System.out.print("Gostaria de tentar novamente ? [s/y] ");
+            pergunta = teclado.next();
+        } while (pergunta.equals("s"));
+        System.out.println("Fim do programa. ");
     }
 }
